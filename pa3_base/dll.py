54d0001388a8e703077d0dc619a758a6e6eb150a
Kristjanbg45 <Kristjangunnarsson1@gmail.com>
2024-02-22 13:41:30 +0000

    nice layout bro
    i love programínk

--- pa3_base/dll.py
+++ pa3_base/dll.py
@@ -10,42 +10,86 @@ class DLL:
         pass
 
     def insert(self, data):
+        """Inserts an item with that value in front of the node at the current position
+                ■ The new node is now in the current position"""
         pass
 
     def remove(self):
+        """Removes the node at the current position if there is one (otherwise does nothing)
+                ■ The node behind the removed node is now in the current position"""
         pass
 
     def get_value(self):
+        """ Returns the value of the item at the current position in the list (None if not item)"""
         return None
 
     def move_to_next(self):
+        """Moves the current position one item closer to the tail/trailer
+                ■ Do nothing if at end of list"""
         pass
 
     def move_to_prev(self):
+        """Moves the current position one item closer to the head/header
+                ■ Do nothing if at beginning"""
         pass
 
     def move_to_pos(self, pos):
+        """Moves the current position to item #position in the list
+                ■ The first actual data item is #0
+                ■ Do nothing if position not between beginning and end (including both)"""
         pass
 
     def clear(self):
+        """Clears all nodes from the list"""
         pass
 
     def get_first_node(self):
+        """Returns the first Node of the list
+                ■ The headers next pointer should be pointing to this node
+                ■ Returns the node, not the value inside it
+            If list is empty, return None"""
         pass
 
     def get_last_node(self):
+        """Returns the last Node of the list
+                ■ The tailers prev pointer should be pointing to this node
+                ■ Returns the node, not the value inside it
+            If list is empty, return None"""
         pass
 
     def partition(self, low, high):
+        """Takes in two nodes from the list as a parameter
+                ■ You can fetch these nodes with get_first_node and get_last_node
+            Uses low as a pivot
+                ■ Loops from low to high and moves all nodes smaller than low so they are
+            ahead(left side) of the low node.
+            Example:
+            ■ List before partition: 10 7 7 14 10 15 1 8 2 4 13 7 11 8 8 13
+            ● Low is 10 which is also a pivot
+            ● High is 13
+            ■ List after partition: 7 7 1 8 2 4 7 8 8 10 14 10 15 13 11 13
+            ■ Note: The list is not sorted but all elements left of 10 are smaller then 10
+            and all elements right of 10 are bigger(or equal)
+            ● The order of elements above and below pivot doesn’t matter, only
+            that they are on the correct side of the pivot
+            After partitioning current position should point towards the pivot
+            Partition will only be tested with valid low and high nodes"""
         pass
 
     def sort(self):
+        """Order the items in the list with any method that uses only your DLL structure
+            ■ No moving everything to another structure, sorting and then moving back!
+            After sorting reset the current position to the beginning of the list
+            5% Bonus for implementing sort using quicksort
+            ■ Partition comes in handy when implementing quicksort"""
         pass
 
     def __len__(self):
+        """Returns the number of items in the list"""
         return 0
 
     def __str__(self):
+        """Returns string with all the items in the list with a single space between them"""
         ret_str = ""
         return ret_str
 
---
+++ pa3_base/dllsolution.py
@@ -0,0 +1,142 @@
+class Node:
+    def __init__(self, data=None, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+class DLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.current = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.current = new_node
+        else:
+            new_node.next = self.current
+            new_node.prev = self.current.prev
+            if self.current.prev:
+                self.current.prev.next = new_node
+            self.current.prev = new_node
+            if self.current == self.head:
+                self.head = new_node
+
+    def remove(self):
+        if self.current:
+            if self.current.prev:
+                self.current.prev.next = self.current.next
+            if self.current.next:
+                self.current.next.prev = self.current.prev
+            if self.current == self.head:
+                self.head = self.current.next
+            if self.current == self.tail:
+                self.tail = self.current.prev
+            self.current = self.current.prev
+
+    def get_value(self):
+        if self.current:
+            return self.current.data
+        else:
+            return None
+
+    def move_to_next(self):
+        if self.current and self.current.next:
+            self.current = self.current.next
+
+    def move_to_prev(self):
+        if self.current and self.current.prev:
+            self.current = self.current.prev
+
+    def move_to_pos(self, pos):
+        current_node = self.head
+        count = 0
+        while current_node and count != pos:
+            current_node = current_node.next
+            count += 1
+        if count == pos:
+            self.current = current_node
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.current = None
+
+    def get_first_node(self):
+        return self.head
+
+    def get_last_node(self):
+        return self.tail
+
+    def partition(self, low, high):
+        pivot = low.data
+        left = low
+        right = low.next
+
+        while right != high.next:
+            if right.data < pivot:
+                left = left.next
+                left.data, right.data = right.data, left.data
+            right = right.next
+
+        low.data, left.data = left.data, low.data
+        self.current = left
+
+    def sort(self):
+        def _quicksort(low, high):
+            if low != high and high != None and low != None and low != high.next:
+                partition_node = self.partition(low, high)
+                _quicksort(low, partition_node.prev)
+                _quicksort(partition_node.next, high)
+
+        _quicksort(self.head, self.tail)
+
+        # Reset current to the beginning of the list
+        self.current = self.head
+
+    def __len__(self):
+        count = 0
+        current_node = self.head
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count
+
+    def __str__(self):
+        ret_str = ""
+        current_node = self.head
+        while current_node:
+            ret_str += str(current_node.data) + " "
+            current_node = current_node.next
+        return ret_str.strip()
+
+if __name__ == "__main__":
+    # Testing the DLL implementation
+    dll = DLL()
+    dll.insert(10)
+    dll.insert(7)
+    dll.insert(7)
+    dll.insert(14)
+    dll.insert(10)
+    dll.insert(15)
+    dll.insert(1)
+    dll.insert(8)
+    dll.insert(2)
+    dll.insert(4)
+    dll.insert(13)
+    dll.insert(7)
+    dll.insert(11)
+    dll.insert(8)
+    dll.insert(8)
+    dll.insert(13)
+    
+    print("List before sorting:")
+    print(dll)
+    
+    dll.sort()
+    
+    print("List after sorting:")
+    print(dll)
